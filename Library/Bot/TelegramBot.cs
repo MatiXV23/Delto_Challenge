5b0fb6ca770bb2549f8bb6a575d7ca95e6b71d8c
Matias Perez <matiperezgordano@gmail.com>
2024-11-18 03:12:28 -0300

    Telegram Bot Added

---
+++ Library/Bot/TelegramBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot.Library.Bot
+{
+    internal class Bot
+    {
+        public ITelegramBotClient botClient;
+
+        public async Task Iniciate()
+        {
+            var config = ConfigReader.Read("../../../Library/Bot/config.json");
+            Console.WriteLine(config.token);
+            botClient = new TelegramBotClient(config.token);
+            var bot = botClient.GetMe().Result;
+
+            Console.WriteLine($"buenas mi id:{bot.Id} y mi nombre: {bot.FirstName}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Telegram.Bot;
+using TelegramBot.Library.Bot;
+
+namespace Program
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bot = new Bot();
+            await bot.Iniciate();
+        }
+    }
+}
